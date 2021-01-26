2106b0c433468dee450686292df4876a53aba7a8
orion3k <10503183@polimi.it>
2021-01-26 23:54:34 +0100

    -Fixed humidity sensor reader script -fixed the controller: fan and humidity controls

--- temp_hum/temp_hum_pub.py
+++ temp_hum/temp_hum_pub.py
@@ -17,32 +17,15 @@ client.on_connect = on_connect
 client.connect("localhost", 1881, 60)
 client.loop_start()
 
-hum_threshold = 70
 last_msg_pub = datetime.now()
 while True:
-	try:
-		humidity,temperature = Adafruit_DHT.read_retry(dht11,dht11_pin,retries=4,delay_seconds=1)
-		if (humidity is not None and temperature is not None):
-			print("T: ",temperature,"H: ",humidity)
-
-			if (humidity < hum_threshold):
-				#nebulizer.on()
-				client.publish("greenhouse/humidity/nebulizer", 1, qos=2)
-				print('Nebulizer ON')
-			else:
-				client.publish("greenhouse/humidity/nebulizer", 0, qos=2)
-				#nebulizer.off()
-				print('Nebulizer OFF')
-
-			if ((datetime.now() - last_msg_pub).total_seconds() > 900. ):
-				client.publish("greenhouse/temperature", temperature)
-				time.sleep(30.)
-				client.publish("greenhouse/humidity",humidity)
-				last_msg_pub = datetime.now()
-			time.sleep(60.)
-	except RuntimeError as error:
+	humidity,temperature = Adafruit_DHT.read_retry(dht11,dht11_pin,retries=4,delay_seconds=1)
+	if (humidity is not None and temperature is not None):
+		print("T: ",temperature,"H: ",humidity)
+		client.publish("greenhouse/temperature", temperature)
+		time.sleep(30.)
+		client.publish("greenhouse/humidity",humidity)
+		last_msg_pub = datetime.now()
+		time.sleep(30.)
+	else:
 		time.sleep(2.0)
-		continue
-	except Exception as Error:
-		raise error
-
